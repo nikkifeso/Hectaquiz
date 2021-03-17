2e8627cedab4665d6c037aaf43223b60eba8be24
Adenike Awofeso <adenikeawofeso@gmail.com>
2021-03-17 14:22:58 +0100

    removed is superadmin

--- quiz/models.py
+++ quiz/models.py
@@ -13,7 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff        = models.BooleanField(default=False)
     # is_superadmin   = models.BooleanField(default=False)
     is_active       = models.BooleanField(default=True)
-    created_at      = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at      = models.DateTimeField(default=timezone.now)
     updated_at      = models.DateTimeField(auto_now=True)
     objects         = UserManager()
 
