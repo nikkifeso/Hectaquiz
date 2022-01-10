0abca344bbc99d15c4a49f934f8e5e359677eb8a
Adenike Awofeso <adenikeawofeso@gmail.com>
2022-01-10 10:49:43 +0100

    fixed docs

--- hectaquiz/urls.py
+++ hectaquiz/urls.py
@@ -9,30 +9,29 @@ from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Hectaquiz App",
-        default_version="v1",
-        description="Welcome to Hectaquiz",
-        terms_of_service="http://example.com/terms/",
-        contact=openapi.Contact(email="adenikeawofeso@gmail.com"),
-        license=openapi.License(name="Awesome IP")
-    ),
-    public=True,
-    permission_classes =(permissions.AllowAny,),
+   openapi.Info(
+      title="Hectaquiz API",
+      default_version='v1',
+      description="Hectaquiz API allows you to create views to our endpoints",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@adenike.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   urlconf="quiz.urls",
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    re_path(r'^doc(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'), 
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'), 
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('quiz.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/rest-auth/', include('rest_auth.urls')),
-    path('api/rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
